189391dcc792e92deac330f70c152384a64a5375
wlinds <w.lindstedt@gmail.com>
2025-09-14 16:25:22 +0200

    Handle hash navigation on page load

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HeroSection from '../components/sections/HeroSection';
 import ServicesSection from '../components/sections/ServicesSection';
 import AboutHempSection from '../components/sections/AboutHempSection';
@@ -9,6 +9,24 @@ import AboutUsSection from '../components/sections/AboutUsSection';
 import ContactSection from '../components/sections/ContactSection';
 
 const LandingPage: React.FC = () => {
+  useEffect(() => {
+    // Handle hash navigation on page load
+    const hash = window.location.hash;
+    if (hash) {
+      setTimeout(() => {
+        const element = document.querySelector(hash);
+        if (element) {
+          const headerHeight = 80;
+          const elementPosition = (element as HTMLElement).offsetTop - headerHeight;
+          window.scrollTo({
+            top: elementPosition,
+            behavior: 'smooth'
+          });
+        }
+      }, 100);
+    }
+  }, []);
+
   return (
     <div className="pt-20">
       <HeroSection />
